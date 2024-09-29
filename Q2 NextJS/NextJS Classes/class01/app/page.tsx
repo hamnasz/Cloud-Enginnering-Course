6aa870cceb2493379dde86198411fc18e005934f
Humna Imran <168326829+hamnasz@users.noreply.github.com>
2024-09-29 16:36:24 +0500

    Hamna Mirza

---
+++ Q2 NextJS/NextJS Classes/class01/app/page.tsx
@@ -0,0 +1,39 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className={styles.text}>
+        <span className={styles.text1}>hello World </span>
+        <strong className={styles.text2}>In Nextjs.</strong>
+      </h1>
+
+      <h1>Difference Between Block-Level and Inline Elements</h1>
+      <div className={styles.blockLevel}>
+        <h2>Block-Level Element</h2>
+        <p className={styles.block}>This is a block-level element.</p>
+        <p className={styles.block}>
+          It occupies its own line and can contain other elements.
+        </p>
+      </div>
+
+      <div className={styles.inlineLevel}>
+        <h2>Inline Element</h2>
+        <span className={styles.inline}>This is an inline element.</span>
+        <span className={styles.inline}>
+          It does not start on a new line and only takes as much width as
+          necessary.
+        </span>
+      </div>
+
+      <p className={styles.differenceExplanation}>
+        Block-level elements like <code>div</code> and <code>p</code> typically
+        start on a new line and stretch out to the full width available. They
+        create a <b>block</b> of content. Inline elements, such as{" "}
+        <code>span</code> and <code>a</code>, only take up as much width as they
+        need and do not break the flow of content. They are used within
+        block-level elements and do not start on a new line.
+      </p>
+    </div>
+  );
+}
---
+++ Q2 NextJS/NextJS Classes/class02/README.md
@@ -0,0 +1,12 @@
+# Class02
+
+In today's class sir teaches us about the basics of html | css | js for it's detail see [html readme.md](./class02-html/README.md)
+
+That's why we're using two different folders today one for html and second for nextjs
+
+for seeing what sir teaches in today's class in next.js see [nextjs readme.md](./class02-nextjs/README.md)
+
+You can check the preview of both websites here
+
+[html basics](https://class02-html-basics.vercel.app/)
+[Tailwind basics in nextjs](https://class-02-tailwind-basics-nextjs.vercel.app/)
